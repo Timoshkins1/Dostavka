4217372f439d974bb67c72c9fbb8797fa7220c15
Pavel Timoshin <163185965+Timoshkins1@users.noreply.github.com>
2025-05-29 11:36:20 +0300

    Initial commit

---
+++ Dostavka/Models/Oreder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dostavka.Models
+{
+    public struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+
+    public class Order
+    {
+        public int ID { get; set; }
+        public Point Destination { get; set; }
+        public double Priority { get; set; }
+    }
+}
